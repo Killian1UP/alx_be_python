63b24426d6364a3e1d52d1d909e21976be379fe1
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2024-09-13 21:30:45 +0200

    Addedmy calculations in the file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum_1 = number1 + number2
+sum_2 = number1 - number2
+sum_3 = number1 * number2
+
+print("Addition of 10 and 5 is " + str(sum_1))
+print("Subtraction of 10 and 5 is " + str(sum_2))
+print("Multiplication of 10 and 5 is " + str(sum_3))
