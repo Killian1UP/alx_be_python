ac1c0f5c4ee2ad7bc5b47e046bf9ea0bb15eec0c
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2024-09-22 16:17:42 +0200

    Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,14 +2,26 @@ task = input("Enter your task: ")
 priority = input("Priority high/medium/low: ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
+p = "is high priority task."
+q = "is medium priority task."
+r = "is low priority task."
+
 match priority:
     case "high":
-        p=print("is high priority task.")
+        print("is high priority task.")
     case "medium":
-        p=print("is medium priority task.")
+        print("is medium priority task.")
     case "low":
-        p=print("is low priority task.")
-if time_bound == "yes":
+        print("is low priority task.")
+if time_bound == "yes" and priority == "high":
     print(f"Reminder: '{task}' {p} that requires immediate attention today!")
-elif time_bound == "no":
+elif time_bound == "yes" and priority == "medium":
+    print(f"Reminder: '{task}' {q} that requires immediate attention today!")
+elif time_bound == "yes" and priority == "low":
+    print(f"Reminder: '{task}' {r} that requires immediate attention today!")
+elif time_bound == "no" and priority == "high":
     print(f"Note: '{task}' {p} Consider completing it when you have free time.")
+elif time_bound == "no" and priority == "medium":
+    print(f"Note: '{task}' {q} Consider completing it when you have free time.")
+elif time_bound == "no" and priority == "low":
+     print(f"Note: '{task}' {r} Consider completing it when you have free time.")
