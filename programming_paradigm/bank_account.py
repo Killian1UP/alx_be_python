312c125d0dd2ddd31e688a81d532cce0a060cc63
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2024-10-06 17:40:09 +0200

    Added Simple Bank Class and program for testing

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,24 +5,16 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.__account_balance += amount
-            print(f"Deposited {amount}: The new balance is {self.__account_balance}")
-        else:
-            print("The amount must be greater than zero!")
-    
+
     def withdraw(self, amount):
         if amount > 0 and self.__account_balance >= amount:
             self.__account_balance -= amount
-            print(f"Withdrawing {amount}: The new balance is {self.__account_balance}")
             return True
+        
         elif amount > self.__account_balance:
-            print("Insufficient Funds! You account does not have enough money.")
-            return False
-
-        else:
-            print("The amount must be greater than zero!")
             return False
             
     def display_balance(self):
-        print(f"Current balance: ${self.__account_balance}")
+        print(f"Current Balance: {self.__account_balance}")
 
 
